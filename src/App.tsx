becb733910e0e1368463ecddc80392b6b6678b01
Tran Viet <tranviet05@192.168.102.6>
2023-09-05 16:58:58 +0700

    update

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
+
+export default App
